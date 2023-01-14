6f3b0deaf84f3b57f0de287ee17687915c0b98d3
Gabriel Tomsic <tomsicgabriel@gmail.com>
2023-01-13 18:11:52 -0700

    Area Calculator Complete

---
+++ src/AriaCalculator.java
@@ -0,0 +1,19 @@
+public class AriaCalculator {
+    public static void main(String[] args) {
+        System.out.println(area(5.0, 4.0));
+    }
+
+    public static double area(double radius){
+        if(radius < 0) {
+            return -1.0;
+        }
+        return (radius * radius) * Math.PI;
+    }
+
+    public static double area(double x, double y) {
+        if(x < 0 || y < 0) {
+            return -1.0;
+        }
+        return x * y;
+    }
+}
